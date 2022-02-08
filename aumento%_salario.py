f9a75f5ec8cf9459577357d4e6a76848638cbf7d
zLuuuck <97802220+zLuuuck@users.noreply.github.com>
2022-02-08 16:46:36 -0300

    Delete aumento%_salario.py

--- aumento%_salario.py
+++
@@ -1,8 +0,0 @@
-sal = float(input('Digite o seu salário(use com "." invés de ","): R$'))
-aumento = float(input('Digite a % do aumento: '))
-novo = sal + (sal * aumento / 100)
-totalau = sal - novo
-
-print(f'Seu salário é de R${sal}. Você ganhou {aumento}% de aumento,', end=' ')
-print(f'agora ele é de R${novo:.2f}')
-print(f'Ganhou-se {totalau}')
