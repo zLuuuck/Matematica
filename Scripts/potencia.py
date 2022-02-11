c04aee3e0c3d7b4762455fdaf5000ebe2ba1f1d5
zLuuuck <97802220+zLuuuck@users.noreply.github.com>
2022-02-11 15:26:36 -0300

    Add files via upload

---
+++ Scripts/potencia.py
@@ -0,0 +1,5 @@
+import math
+num = int(input('Digite o número: '))
+ex = int(input('Digite o expoente: '))
+pot = math.pow(num, ex)
+print(f'O resultado de sua conta é {pot}')
---
+++ Scripts/raiz4.py
@@ -0,0 +1,4 @@
+import math
+num = int(input('Digite o número: '))
+raiz = math.sqrt(num)
+print(f'O resultado de sua conta é {raiz}')
